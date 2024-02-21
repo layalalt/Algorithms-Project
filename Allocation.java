b89692ef131df6f65260421886702efb371e6956
layalalt <147754472+layalalt@users.noreply.github.com>
2024-02-22 00:59:27 +0300

    Allocation.java

--- Allocation.java
+++ Allocation.java
@@ -1,16 +1,15 @@
 public class Allocation
 {
    int asset1, asset2, asset3;
-   double retrn, risk;
+   double aReturn, risk;
    
-   public void setAllocation(int asset1, int asset2, int asset3, double retrn, double risk)
+   public void setAllocation(int asset1, int asset2, int asset3, double aReturn, double risk)
    { 
      this.asset1 = asset1;
      this.asset2 = asset2;
      this.asset3 = asset3;
-     this.retrn = retrn;
+     this.aReturn = aReturn;
      this.risk = risk;
    }
    
 }
-  
